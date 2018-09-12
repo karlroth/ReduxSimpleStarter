9ae1e494fb79e2b0e4c85735f68d7f2b70c37ec7
Karl Roth <karoth4@illinois.edu>
2018-09-11 21:16:55 -0500

    added input functionality

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDNcwJwggkWXlgI_12RV5WKCWszSAJ0s7M'
+
 // Create a new component. This component should produce some HTML.
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 // Take this component's generated HTML 
 // and put it on the page (in the DOM)
-ReactDOM.render(<App/>, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
