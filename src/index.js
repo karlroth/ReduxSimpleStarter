45e8d4d8851af3b5ad84f4307016e7d9a6a8c43b
Karl Roth <karoth4@illinois.edu>
2018-09-12 18:00:24 -0500

    Added youtube-search-api functionality to the index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyDNcwJwggkWXlgI_12RV5WKCWszSAJ0s7M'
 
-// Create a new component. This component should produce some HTML.
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos });
+            // this.setState({ videos: videos });
+        });
+    }
+
+    render() {
+
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
-// Take this component's generated HTML 
-// and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
